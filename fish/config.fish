d95d10c1f5d6455a14b4c33d76a88467fc80106b
Flambynou <tom.ghislain@ik.me>
2026-06-09 12:14:19 +0200

    Big udpates

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,4 @@
 if test (tty) = /dev/tty1
-    if uwsm check may-start
-        exec uwsm start hyprland.desktop
-    end
+    exec start-hyprland
 end
 set -g fish_greeting
-if not set -q SSH_AUTH_SOCK
-    ssh-agent -c | source
-    ssh-add ~/.ssh/id_ed25519_uclouvain
-end
